7475d121ecd2d6fcdf5efae1257127bd917f80b5
Mimir Dev <dev@mimir.local>
2026-05-25 15:24:03 -0500

    chore: align homebrew checksums with v1.0.0 release assets

--- HomebrewFormula/mimir.rb
+++ HomebrewFormula/mimir.rb
@@ -6,20 +6,20 @@ class Mimir < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/LivingEthos/mimir/releases/download/v1.0.0/mimir-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "7d6e364ee6b77e0a141048d0dcb6a994c12b8813702f8daf34ca79d529e68db7"
+      sha256 "11fff039ebdce467d25b88727166ed0d659f5371e354a169643a4a6da722db8a"
     else
       url "https://github.com/LivingEthos/mimir/releases/download/v1.0.0/mimir-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "c39216591bc8540ef60c68af490014870636696ed378518e1971743b3a6e4f64"
+      sha256 "13b2ab4d7f47e1e76a719e87bb64896c944441a75cef5a5d78ec6a5ead94d996"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/LivingEthos/mimir/releases/download/v1.0.0/mimir-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "2f0702bbac0a3f6ac5e8fd4b74d2d3528a27867b03d01f3979884219bb4dac59"
+      sha256 "bafc80ac1ea718f0a667d35b9682b929e8222aafe5914ea671a210bac0fd582b"
     else
       url "https://github.com/LivingEthos/mimir/releases/download/v1.0.0/mimir-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "528a17f761169eb013cd79d2429e5cf74db30c088aa87f1cd90acfb0fb6f3256"
+      sha256 "9c32100447e7ab62425107874eb41ea4722c6f578183b3cb63cd91ab360ba13d"
     end
   end
 
